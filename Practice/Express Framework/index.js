5123c61720cf5307ce93f0e4dab18a928da2c165
ayushsahu01 <ayushsahu29042005@gmail.com>
2026-02-02 11:49:35 +0530

    [Ayush] Add . Express Work on 020226

--- Practice/Express Framework/index.js
+++ Practice/Express Framework/index.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const app = express();
 const PORT = 7054;
-app.get("/", (req, res) => {
+
+const students = [
+    {id: 1, name: "Ayush", branch: "CSE"},
+    {id: 2, name: "Anuj", branch: "Managment"},
+    {id: 3, name: "Priyanshu", branch: "BCA"}
+]
+
+app.get("/", (req, res)=>{
     res.send("Welcome to Home Page");
 })
-app.get("/users", (req, res) => {
-    res.send("<h1>This is Users Page<h1>")
+
+app.get("/students", (req,res)=>{                //http://localhost:7054/students?branch=CSE
+    const branch = req.query.branch;
+    const foundStudent = students.filter(s=>s.branch==branch);
+    res.json(foundStudent);
 })
-app.get("/users/:id", (req, res) => {
-    const userId = req.params.id
-    res.send(`You are requesting for user:${userId}`)
+
+app.get("/students", (req, res)=>{
+    res.json(students);
 })
+
+app.get("/students/:id", (req, res)=>{
+    const id = req.params.id;
+    const arrayIndex = students.findIndex(s=> s.id==id);
+    const data = students[arrayIndex];
+    res.json(data);
+})
+
+// app.get("/", (req, res) => {
+//     res.send("Welcome to Home Page");
+// })
+// app.get("/users", (req, res) => {
+//     res.send("<h1>This is Users Page<h1>")
+// })
+// app.get("/users/:id", (req, res) => {
+//     const userId = req.params.id
+//     res.send(`You are requesting for user:${userId}`)
+// })
 app.listen(PORT, () => {
     console.log(`Server is Running on Port:${PORT}`);
 }) 
