e094acd5fbe4fb16c34a1160e7183c6e05d807ee
ayushsahu01 <ayushsahu29042005@gmail.com>
2026-02-11 10:36:51 +0530

    [Ayush] Add . Middleware routers

--- Practice/Express Framework/Middleware Routes/middlewareRouter.js
+++ Practice/Express Framework/Middleware Routes/middlewareRouter.js
@@ -10,29 +10,81 @@ app.listen(PORT, () => {
 });
 
 app.use((req,res,next)=>{
- console.log("i m middleware 1");
+ console.log("i m middleware");
  next();
-})
+});
 
-app.use((req,res,next)=>{
- console.log("i m middleware 2");
- next();
-})
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization;
 
-const loggerFile=(req,res,next)=>{
-  const log=`Request at:${new Date().toLocaleString()} method:${req.method}`
-  fs.appendFile("./log.txt",log ,(err)=>{
-    if(err){
-      console.log(err);
-    }
-  });
+  console.log("Token:", token);
+
+  if (!token) {
+    return res.status(401).json({ message: "Token missing" });
+  }
+
+  if (token !== "Bearer mysecrettoken") {
+    return res.status(403).json({ message: "Invalid token" });
+  }
+
   next();
 };
 
-app.get("/students/",loggerFile, async(req, res) => {
-    const students= await readStudentsFromFile();
-    return res.status(200).json(students);
-})
+
+app.put("/students/:id", authMiddleware, async (req, res) => {
+  try {
+    const userId = parseInt(req.params.id);
+
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).json({ message: "Empty body not allowed" });
+    }
+
+    const students = await readStudentsFromFile();
+
+    const index = students.findIndex(s => s.id === userId);
+    if (index === -1) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    students[index] = { ...students[index], ...req.body };
+
+    await writeStudentsToFile(students);
+
+    res.status(200).json({
+      message: "Updated successfully",
+      student: students[index]
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+});
+
+
+app.use(async (req, res, next) => {
+  try {
+    const log = `${new Date().toLocaleString()} | ${req.method} | ${req.url}\n`;
+    await fs.appendFile("./log.txt", log);
+    next();
+  } catch (err) {
+    console.error("Log error:", err.message);
+    next();
+  }
+});
+
+// app.use((req,res,next)=>{
+//   const log=`Request at:${new Date().toLocaleString()} method:${req.method}`
+//   fs.appendFile("./log.txt","utf-8",(err)=>{
+//     if(err){
+//       console.log(err);
+//     }
+//   });
+//   next();
+// });
+
+// app.get("/students/",loggerFile, async(req, res) => {
+//     const students= await readStudentsFromFile();
+//     return res.status(200).json(students);
+// })
 
 const readStudentsFromFile = async () => {
   const data = await fs.readFile("./students.json", "utf-8");
